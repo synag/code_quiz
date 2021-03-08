52eb7e0aec42cada55f2b66067dd1e1128c58ea4
Carl Benbow <cbenbow@synergisticagility.com>
2021-03-07 23:02:01 -0500

    updated files and created functions

---
+++ assets/js/code_quiz.js
@@ -0,0 +1,48 @@
+//Selectors goes here
+var openScreen = document.querySelector("#box-title");
+var openScreenPTag = document.querySelector("#game-description");
+
+
+//Global Variables
+
+var openTitle = "Coding Quiz Challenge";
+var description = "Try to answer the following code-related question within the time limit. Keep in mind that the incorrect answer will penalize your scoreTime by ten seconds!"
+
+
+
+//Functions 1 - Opening Screen
+function startScreen(){
+    
+         openScreen.textContent = openTitle;
+
+      console.log(openTitle)  
+ 
+// openScreenPtag.textContent = description;
+
+// console.log(openTitle)
+}
+
+
+
+
+//TextContent
+
+
+//Create an append button below ptag
+
+//Call Functions
+
+startScreen()
+
+
+
+
+
+
+
+//SetAttributes
+
+
+
+
+//Append 
