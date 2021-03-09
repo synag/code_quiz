6ebd7512665b08c7093610e86498b0b450034e8b
Carl Benbow <cbenbow@synergisticagility.com>
2021-03-08 23:28:59 -0500

    updated js

--- assets/js/code_quiz.js
+++ assets/js/code_quiz.js
@@ -1,48 +1,68 @@
 //Selectors goes here
-// var openScreen = document.querySelector("#box-title").innerText = "hello";
-// var openScreenPTag = document.querySelector("#game-description");
+var openScreen = document.querySelector("#box-title")
+var openScreenPTag = document.querySelector('p');
+var anchor = document.querySelector("a")
+var button = document.querySelector('start')
+var input = document.querySelector(".input")
+
 
 
 //Global Variables
+var timer =""
+var score = ""
 
-var openTitle = "Coding Quiz Challenge";
-var description = "Try to answer the following code-related question within the time limit. Keep in mind that the incorrect answer will penalize your scoreTime by ten seconds!"
+var quizes = [ 
+    {
+    questions: "what is a string ?",
+    answer: {
+        a: "number",
+        b: "string",
+        c: "booleon"
+    },
+    correctAnswer: "string"
 
+},
+{
+questions: "what is a xys ?",
+    answer: {
+        a: "numbers",
+        b: "stringw",
+        c: "booleon"
+    },
+    correctAnswer: "string"
 
+},
 
-// //Functions 1 - Opening Screen
-// function startScreen(){
-    
-//         openScreen = openTitle;
-
-//       console.log(openTitle)  
- 
-// // openScreenPtag.textContent = description;
-
-// // console.log(openTitle)
-// }
-
-
-
-
-// //TextContent
-
-
-// //Create an append button below ptag
-
-// //Call Functions
-
-// startScreen()
+]
 
 
 
+function defaultPage(){
+    openScreen ="Coding Quiz Challenge"
+    openScreen.innerText;
+
+   openScreenPTag.innerText ="Try to answer the following code-related question within the time limit. Keep in mind that the incorrect answer will penalize your scoreTime by ten seconds!";
+
+
+     
+}
+
+//Funtion init
+//Timer function
+//startQuiz
+//execute quiz
+
+//store results function
+localStorage.setItem("todos", JSON.stringify(todos));
+
+//get result function
+var storedTodos = JSON.parse(localStorage.getItem("todos"));
 
 
 
 
-//SetAttributes
 
 
 
 
-//Append 
+
