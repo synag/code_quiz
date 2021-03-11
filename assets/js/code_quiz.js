cf4390e2f215f55d56dd20f2a42b5430be4d4ae4
Carl Benbow <cbenbow@synergisticagility.com>
2021-03-10 23:03:16 -0500

    created the start function

--- assets/js/code_quiz.js
+++ assets/js/code_quiz.js
@@ -2,13 +2,14 @@
 var openScreen = document.querySelector("#box-title")
 var openScreenPTag = document.querySelector('p');
 var anchor = document.querySelector("a")
-var button = document.querySelector('start')
+var button = document.querySelector('#startBtn')
 var input = document.querySelector(".input")
+var timeEl = document.querySelector("#timer");
 
 
 
 //Global Variables
-var timer =""
+var secondsLeft = 75;
 var score = ""
 
 var quizes = [ 
@@ -36,23 +37,61 @@ questions: "what is a xys ?",
 ]
 
 
-/
-//startQuiz Hide Elements
+
+
+
+//start Function Hides the default page 
+
+function start() {
+
+openScreen.style.display = "none";
+openScreenPTag.style.display = "none";
+button.style.display = "none";
+
+}
 
 //Funtion init
-//Timer function
 
+
+// Selects element by class
+
+
+// Selects element by id
+
+
+//Timer Function
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+    //  sendMessage();
+    }
+  }, 1000);
+}
+
+//game over function
 //execute quiz
 
 //store results function
-localStorage.setItem("todos", JSON.stringify(todos));
+// localStorage.setItem("todos", JSON.stringify(todos));
 
 //get result function
-var storedTodos = JSON.parse(localStorage.getItem("todos"));
+// var storedTodos = JSON.parse(localStorage.getItem("todos"));
 
 
 
-
+//startQuiz Hide Elements
+// setTime();
+button.addEventListener("click", function() {
+   //set attribute to display none
+});
+setTime();
 
 
 
